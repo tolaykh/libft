bf8763495812d57d54a49d6e05129139ca74241b
tolaykh <Tolaykh@outlook.com>
2025-08-08 00:56:45 +0300

    Update ft_strncmp.c

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -10,16 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
-
-	i = 0;
-	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		++i;
-	}
-	return (0);
-}
+    while (n--)
+    {
+        unsigned char c1 = (unsigned char)*s1++;
+        unsigned char c2 = (unsigned char)*s2++;
+        if (c1 != c2)
+            return (c1 - c2);
+        if (c1 == 0)
+            break;
+    }
+    return 0;
