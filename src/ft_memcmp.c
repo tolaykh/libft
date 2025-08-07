31f09ac894eb63a5489a2529b1640c61c6818198
tolaykh <Tolaykh@outlook.com>
2025-08-07 22:26:30 +0300

    Create ft_memcmp.c

---
+++ src/ft_memcmp.c
@@ -0,0 +1,17 @@
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
+	while(n)
+	{
+		if(*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+		ptr1++;
+		ptr2++;
+		n--;
+	}
+	return 0;
+}
