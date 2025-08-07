459dcb16a5f49e9ceca51fb1d667776ed802f074
tolaykh <Tolaykh@outlook.com>
2025-08-07 11:30:37 +0300

    Update libft.h

---
+++ include/libft.h
@@ -0,0 +1,12 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+// Function prototypes
+size_t  ft_strlen(const char *s);
+void    ft_putchar_fd(char c, int fd);
+void    *ft_memcpy(void *dst, const void *src, size_t n);
+
+#endif
