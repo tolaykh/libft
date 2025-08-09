ac300711e36c2dc78c0f525919010fb9ef0a49a6
tolaykh <Tolaykh@outlook.com>
2025-08-09 14:20:34 +0300

    Update ft_memchr.c

--- src/ft_memchr.c
+++ src/ft_memchr.c
@@ -1,4 +1,16 @@
-void	*memchr(const void *s, int c, size_t n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkhamis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/09 12:54:49 by tkhamis           #+#    #+#             */
+/*   Updated: 2025/08/09 12:54:59 by tkhamis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	const unsigned char 	*ptr;
 	ptr = (const unsigned char *)s;
@@ -11,4 +23,3 @@ void	*memchr(const void *s, int c, size_t n)
 	}
 	return NULL;
 }
-
