1d7fe3f18347cb763ecf9b81152a8dca866c750c
tolaykh <Tolaykh@outlook.com>
2025-08-09 14:22:01 +0300

    Update ft_memcpy.c

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -6,20 +6,20 @@
 /*   By: tkhamis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 12:14:02 by tkhamis           #+#    #+#             */
-/*   Updated: 2025/08/06 12:33:17 by tkhamis          ###   ########.fr       */
+/*   Updated: 2025/08/06 18:10:21 by tkhamis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char	*w;
+	unsigned char		*w;
 	const unsigned char	*s;
 
 	w = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-	while(n)
+	while (n)
 	{
-		*w++ = *s++; 
+		*w++ = *s++;
 		n--;
 	}
 	return (dest);
