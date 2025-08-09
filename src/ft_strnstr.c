555af8f8f0a1fa719fc010c439080b427bb3d3fa
tolaykh <Tolaykh@outlook.com>
2025-08-09 14:25:48 +0300

    Update ft_strnstr.c

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -1,4 +1,16 @@
-char *strnstr(const char *hay, const char *needle, size_t n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkhamis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/09 12:57:10 by tkhamis           #+#    #+#             */
+/*   Updated: 2025/08/09 12:57:21 by tkhamis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char *ft_strnstr(const char *hay, const char *needle, size_t n)
 {
 	size_t i = 0;
 	size_t j = 0;
