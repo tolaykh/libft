8d6e49e54a56dfc327aaf51e534e05d8a57b0db5
Tolay Khamis <tkhamis@c2r2s3.42amman.com>
2025-08-15 17:31:17 +0300

    update

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,24 @@
 /*   By: tkhamis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 14:00:38 by tkhamis           #+#    #+#             */
-/*   Updated: 2025/08/09 14:19:19 by tkhamis          ###   ########.fr       */
+/*   Updated: 2025/08/15 17:27:39 by tkhamis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <limits.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	total_size;
 	void	*ptr;
 
-	if (nmemb || size == 0)
-		return (ptr);
+	if (nmemb == 0 || size == 0)
+	{
+		nmemb = 1;
+		size = 1;
+	}
 	total_size = nmemb * size;
 	ptr = malloc(total_size);
 	if (ptr != NULL)
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,28 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkhamis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: tkhamis <tkhamis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 16:14:21 by tkhamis           #+#    #+#             */
-/*   Updated: 2025/08/15 16:34:05 by tkhamis          ###   ########.fr       */
+/*   Updated: 2025/08/15 16:55:44 by tkhamis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	if(!s1 || !s2)
+	char	*str;
+	int		s1_len;
+	int		s2_len;
+	int		i;
+
+	if (!s1 || !s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-
-	char *str = malloc(s1_len + s2_len + 1);
+	str = malloc(s1_len + s2_len + 1);
 	if (!str)
-		return 	(NULL);
+		return (NULL);
 	ft_memcpy(str, s1, s1_len);
-	while(str[s1_len + 1])
+	i = 0;
+	while (i < s2_len)
 	{
-		str[s1_len + 1] = 
+		str[s1_len + i] = s2[i];
+		i++;
 	}
+	str[s1_len + s2_len] = '\0';
+	return (str);
 }
