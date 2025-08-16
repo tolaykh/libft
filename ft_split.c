f8c7c03dbf9eecf4bd5ba47ce950353c3c12681d
tolaykh <Tolaykh@outlook.com>
2025-08-17 00:19:40 +0300

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -30,24 +30,61 @@ int count_words(const char *s, char c)
 	return (count);
 }
 
+char *word_dup(const char *s, int len)
+{
+    char *word;
+    int i = 0;
+
+    word = malloc(sizeof(char) * (len + 1));
+    if (!word)
+        return (NULL);
+    while (i < len)
+    {
+        word[i] = s[i];
+        i++;
+    }
+    word[i] = '\0';
+    return (word);
+}
+void free_i(char **res, int i)
+{
+    while (i >= 0)
+    {
+        free(res[i]);
+        i--;
+    }
+    free(res);
+}
+
 char **ft_split(char const *s, char c)
 {
+	int len;
+	int i = 0;
 	char **res;
-	char start;
+	char *start;
 	int words = count_words(s, c);
 	
+	if(!s)
+		return (NULL);
 	res = malloc(sizeof(char *) * (words + 1));
+	if(!res)
+		return (NULL);
 	while (*s)
 	{
 		if(*s != c)
 		{
 			start = s;
-		while(*s && *s != c)
-			s++;
-		///	
+			while(*s && *s != c)
+				s++;
+			len = s - start;
+			res[i] = word_dup(start, len);
+			if (!res[i])
+				return (free_i(res, i - 1), NULL);
+			i++;
 		}
 		else
 			s++;
 	}
-
+	res[i] = NULL;
+	return (res);
 }
